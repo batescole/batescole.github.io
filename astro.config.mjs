13b2d803860af802ebe210e70759c16546d46375
Cole Bates <bates.cole@gmail.com>
2025-10-06 16:34:53 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/docs',
+  site: 'http://batescole.github.io/',
   vite: {
     plugins: [tailwindcss()],
   },
